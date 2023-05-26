5d1d1ea16f36aa3ffd2d38d3e61159032d7f8668
Jaime7w7r <jaimeadolfov07@gmail.com>
2023-05-26 15:03:37 -0600

    PWA

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
 const coffees = [
     { name: "Perspiciatis", image: "images/coffee.jpg" },
     { name: "Voluptatem", image: "images/coffee.jpg" },
@@ -9,32 +9,41 @@ const coffees = [
     { name: "Inventore", image: "images/coffee.jpg" },
     { name: "Veritatis", image: "images/coffee.jpg" },
     { name: "Accusantium", image: "images/coffee.jpg" },
-]
+];
 
 const showCoffees = () => {
-    let output = ""
-    coffees.forEach(
-        ({ name, image }) =>
-        (output += `
-                <div class="card">
-                  <img class="card--avatar" src=${image} />
-                  <h1 class="card--title">${name}</h1>
-                  <a class="card--link" href="#">Taste</a>
-                </div>
-                `)
-    )
-    container.innerHTML = output
-}
+    let output = "";
+    coffees.forEach(({ name, image }) => {
+        output += `
+      <div class="card">
+        <img class="card--avatar" src=${image} />
+        <h1 class="card--title">${name}</h1>
+        <a class="card--link" href="#">Taste</a>
+      </div>
+    `;
+    });
+    container.innerHTML = output;
+};
 
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function () {
-        navigator.serviceWorker
-            .register("/serviceWorker.js")
-            .then(res => console.log("service worker registered"))
-            .catch(err => console.log("service worker not registered", err))
-    })
-}
+let deferredPrompt;
 
+window.addEventListener('beforeinstallprompt', function (event) {
+    event.preventDefault();
+    deferredPrompt = event;
+});
 
+window.addEventListener('load', function () {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/serviceWorker.js').then(function (registration) {
+            console.log('Service Worker registered successfully:', registration);
 
-document.addEventListener("DOMContentLoaded", showCoffees)
+            if (deferredPrompt) {
+                alert('¡Instala nuestra aplicación!');
+            }
+        }).catch(function (error) {
+            console.log('Error registering Service Worker:', error);
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', showCoffees);
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "dev-coffee-site-v1"
+const staticDevCoffee = "dev-coffee-site-v1";
 const assets = [
     "/",
     "/index.html",
@@ -12,26 +12,35 @@ const assets = [
     "/images/coffee.jpg",
     "/images/coffee.jpg",
     "/images/coffee.jpg",
-    "/images/coffee.jpg",
-]
+    "/images/coffee.jpg"
+];
 
 self.addEventListener("install", installEvent => {
     installEvent.waitUntil(
         caches.open(staticDevCoffee).then(cache => {
-            cache.addAll(assets)
+            cache.addAll(assets);
         })
-    )
+    );
     self.skipWaiting(); // Agrega esta línea para activar el Service Worker inmediatamente
-})
+});
 
 self.addEventListener("activate", activateEvent => {
     activateEvent.waitUntil(
         // Elimina las caches antiguas si existen
         caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.filter(cacheName => cacheName !== staticDevCoffee)
+                cacheNames
+                    .filter(cacheName => cacheName !== staticDevCoffee)
                     .map(cacheName => caches.delete(cacheName))
             );
         })
     );
-})
+});
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request);
+        })
+    );
+});
